3a98853c77451fb146d1efeb20df567f3bfb4986
Ravi Rajput <ravi.rajput@contractors.roche.com>
2025-11-06 23:16:26 +0530

    added db manifests for app

--- k8s/db/schema.sql
+++ k8s/db/schema.sql
@@ -1,2 +1,2 @@
 CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, bio TEXT);
-INSERT INTO users (name, bio) VALUES ('alice', 'developer'), ('bob','ops');
+INSERT INTO users (name, bio) VALUES ('Ravi', 'DevSecOps'), ('Prashant','Developer');
